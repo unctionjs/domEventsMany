45450d3e0ae59904d30e37cfdd1d91885d59f545
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:33:11 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-import mergeRight from "@unction/mergeright"
-import reduceValues from "@unction/reducevalues"
-import mapValues from "@unction/mapvalues"
-import domEvents from "@unction/domevents"
-import {empty} from "most"
-
-export default function domEventsMany (options: DOMEventsConfigurationType): UnaryFunctionType {
-  const domEventsWithOptions: UnaryFunctionType = domEvents(options)
-
-  return function domEventsManyOptions (names: $ReadOnlyArray<DOMEventNameType>): UnaryFunctionType {
-    return function domEventsManyOptionsNames (dom: DOMStreamType): DOMEventStreamType {
-      return reduceValues(
-        mergeRight
-      )(
-        empty()
-      )(
-        mapValues((name: DOMEventNameType): DOMEventStreamType => domEventsWithOptions(name)(dom))(names)
-      )
-    }
-  }
+import mergeRight from "@unction/mergeright";
+import reduceValues from "@unction/reducevalues";
+import mapValues from "@unction/mapvalues";
+import domEvents from "@unction/domevents";
+import { empty } from "most";
+export default function domEventsMany(options) {
+  const domEventsWithOptions = domEvents(options);
+  return function domEventsManyOptions(names) {
+    return function domEventsManyOptionsNames(dom) {
+      return reduceValues(mergeRight)(empty())(mapValues(name => domEventsWithOptions(name)(dom))(names));
+    };
+  };
 }
