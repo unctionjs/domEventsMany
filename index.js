c40d49abe25a55ff15315e93ce50e966fe051ea9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:42 -0700

    Autolinting

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import mergeRight from "@unction/mergeright";
 import reduceValues from "@unction/reducevalues";
 import mapValues from "@unction/mapvalues";
 import domEvents from "@unction/domevents";
-import { empty } from "most";
-export default function domEventsMany(options) {
+import {empty} from "most";
+export default function domEventsMany (options) {
   const domEventsWithOptions = domEvents(options);
-  return function domEventsManyOptions(names) {
-    return function domEventsManyOptionsNames(dom) {
-      return reduceValues(mergeRight)(empty())(mapValues(name => domEventsWithOptions(name)(dom))(names));
+
+
+  return function domEventsManyOptions (names) {
+    return function domEventsManyOptionsNames (dom) {
+      return reduceValues(mergeRight)(empty())(mapValues((name) => domEventsWithOptions(name)(dom))(names));
     };
   };
 }
--- test.js
+++ test.js
@@ -1,26 +1,26 @@
 /* eslint-disable flowtype/require-variable-type */
-import {test} from "tap"
-import {empty} from "most"
-import {of} from "most"
-import streamSatisfies from "@unction/streamsatisfies"
+import {test} from "tap";
+import {empty} from "most";
+import {of} from "most";
+import streamSatisfies from "@unction/streamsatisfies";
 
-import domEventsMany from "./index"
+import domEventsMany from "./index";
 
 const dom = {
   events: (type) => {
     switch (type) {
       case "click": {
-        return of({type: "click"})
+        return of({type: "click"});
       }
       case "input": {
-        return of({type: "input"})
+        return of({type: "input"});
       }
       default: {
-        return empty()
+        return empty();
       }
     }
   },
-}
+};
 
 test(({equal, same, doesNotThrow, end}) => {
   streamSatisfies(
@@ -35,10 +35,10 @@ test(({equal, same, doesNotThrow, end}) => {
   )(
     ({length}) =>
       (position) => {
-        equal(length, position)
-        end()
+        equal(length, position);
+        end();
       }
   )(
     domEventsMany({})(["click", "input"])(dom)
-  )
-})
+  );
+});
